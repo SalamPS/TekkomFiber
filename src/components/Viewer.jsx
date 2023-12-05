51c6f504f48c1e08b26b7747a880f109935abf71
LamP <senzawa.g@gmail.com>
2023-12-05 09:55:11 +0700

    feat: Added links from origin, Updating buttons

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -4,11 +4,12 @@ import Box from "../canvas/Box";
 import { useState } from "react";
 
 class object {
-  constructor (type,name,desc,prop) {
+  constructor (type,name,desc,prop,origin) {
     this.type = type;
     this.name = name;
     this.desc = desc;
     this.prop = prop;
+    this.origin = origin;
   }
 }
 
@@ -20,7 +21,8 @@ const list = [
     pos: [-1, 0, 3],
     rot: [0, 0, 0],
     src: 'asus_rog_geforce_rtx_VGA_CARD'
-  }),
+  },"https://sketchfab.com/3d-models/asus-rog-geforce-rtx-4090-v20-6f527569f14b4efc94c7072842bd41ac"
+  ),
   new object('monitor', 'Asus PC Gaming Monitor', 'Gaming Monitor', {
     scale: 15,
     speed: [0, 0, 0],
@@ -28,7 +30,8 @@ const list = [
     pos: [0, -5, 0],
     rot: [-0.3, 3.95, -0.20],
     src: 'asus_pc_gaming_monitor'
-  }),
+  },"https://sketchfab.com/3d-models/asus-pc-gaming-monitor-941ce968c30a42d0a640a5c9d7f56128"
+  ),
   new object('motherboard', 'Asus Strix B550-F Gaming Motherboard', 'Gaming Motherboard', {
     scale: 0.8,
     speed: [0, 0, 0],
@@ -36,7 +39,8 @@ const list = [
     pos: [0, -13, 0],
     rot: [1, 0.6, -0.7],
     src: 'asus_strix_b_550_f_gaming_motherboard_realistic'
-  }),
+  },"https://sketchfab.com/3d-models/asus-strix-b-550-f-gaming-motherboard-realistic-3eba5f45bed74fbeb2647de38047000f"
+  ),
   new object('ram', 'Crucial Ballistix 8GB DDR4 3600 RAM (Black)', 'DDR4 RAM', {
     scale: 200,
     speed: [0, 0, 0],
@@ -44,7 +48,8 @@ const list = [
     pos: [0, 0, 0],
     rot: [0, 0, 0],
     src: 'crucial_ballistix_8gb_ddr4_3600_ram__black'
-  }),
+  },"https://sketchfab.com/3d-models/crucial-ballistix-8gb-ddr4-3600-ram-black-1fce4935471e46cab6ee57ba140c87f9"
+  ),
   new object('cpu', 'Dream Computer CPU', 'Computer CPU', {
     scale: 1,
     speed: [0, 0, 0],
@@ -52,7 +57,8 @@ const list = [
     pos: [0, 0, 0],
     rot: [0, 0, 0],
     src: 'dream_computer_CPU'
-  }),
+  },"https://sketchfab.com/3d-models/dream-computer-setup-82f78bbaf2d34f01af854a52151dbf49"
+  ),
   new object('mouse', 'Gaming Mouse', 'Gaming Mouse', {
     scale: 1,
     speed: [0, 0, 0],
@@ -60,7 +66,8 @@ const list = [
     pos: [0, 0, 0],
     rot: [0, 0, 0],
     src: 'gaming_mouse'
-  }),
+  },"https://sketchfab.com/3d-models/gaming-mouse-ab744476bdb343a9bf4544db12168af6"
+  ),
   new object('keyboard', 'Mechanical RGB Keyboard', 'RGB Keyboard', {
     scale: 0.3,
     speed: [0.0005, 0, 0],
@@ -68,7 +75,8 @@ const list = [
     pos: [0, 0, 0],
     rot: [0, 0, 0],
     src: 'mechanical_rgb_keyboard'
-  }),
+  },"https://sketchfab.com/3d-models/mechanical-rgb-keyboard-4650f5bafe934a90b9f09396b843a966"
+  ),
   new object('cooler', 'PC Cooler 2', 'Cooling System', {
     scale: 1,
     speed: [0, 0, 0],
@@ -76,7 +84,8 @@ const list = [
     pos: [0, 0, 0],
     rot: [0, 0, 0],
     src: 'pc_cooler_2'
-  }),
+  },"https://sketchfab.com/3d-models/pc-cooler-2-5cc52b3d32064c9ca6d476e1a90b634f"
+  ),
   new object('speaker', 'Speaker Vipe Nitro X7', 'Gaming Speaker', {
     scale: 1,
     speed: [0, 0, 0],
@@ -84,7 +93,8 @@ const list = [
     pos: [0, 0, 0],
     rot: [0, 0, 0],
     src: 'speaker_vipe_nitro_x7'
-  }),
+  },"https://sketchfab.com/3d-models/speaker-vipe-nitro-x7-f8ad99da408f4211b224ef51f08cee06"
+  ),
   new object('ssd', 'SSD Samsung 980 Pro 1TB', 'Solid State Drive', {
     scale: 1,
     speed: [0, 0, 0],
@@ -92,7 +102,8 @@ const list = [
     pos: [0, 0, 0],
     rot: [0, 0, 0],
     src: 'ssd_samsung_980_pro_1tb'
-  }),
+  },"https://sketchfab.com/3d-models/ssd-samsung-980-pro-1tb-208b846d5a5948368b4ad0676bf4ab97"
+  ),
   new object('desktop', 'Desktop PC', 'Personal Computer', {
     scale: 1,
     speed: [0, 0, 0],
@@ -100,7 +111,8 @@ const list = [
     pos: [0, 0, 0],
     rot: [0, 0, 0],
     src: 'desktop_pc'
-  })
+  },"https://sketchfab.com/3d-models/tforce-delta-ram-aee3bb8d00ff409986aca6009b91d5e8"
+  )
 ];
 
 const View = () => {
@@ -117,15 +129,15 @@ const View = () => {
     <div id="control">
       <div>
         <button id="toggleSpeed" onClick={toggleSpeed} className={(prop.speed.some(speedValue => speedValue !== 0) ? "show" : "hide")}>
-          {toggle ? 'Turn Off Rotation' : 'Turn On Rotation'}
+          <span className={`bi bi-toggle-${toggle ? "on" : "off"}`}></span>
         </button>
       </div>
       <div id="navigate">
-        <button title="info">
-          <span className="bi bi-link-45deg"></span>
+        <button title="control">
+          <span className="bi bi-dpad"></span>
         </button>
-        <button title="link">
-          <span className="bi bi-link-45deg"></span>
+        <button title="link" onClick={() => window.open(show.origin, '_blank')}>
+          <span className="bi-arrow-up-right-circle"></span>
         </button>
       </div>
     </div>
