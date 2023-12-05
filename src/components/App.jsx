cd6587a1f5b46d3010e8145f4c3c6f163fad0bf9
LamP <senzawa.g@gmail.com>
2023-12-05 12:57:45 +0700

    refactor: Deleted unused 3Dloader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,97 +1,24 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
-import 'bootstrap-icons/font/bootstrap-icons.css';
-// import { useRef } from 'react';
-// import { Canvas, extend, useFrame, useThree } from '@react-three/fiber';
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-// import * as THREE from 'three'; // Import THREE
 
-// extend({ OrbitControls });
-
-// const Model = ({src, speed, scale, cam}) => {
-//   const group = useRef();
-
-//   useFrame((state, delta) => {
-//     if (group.current.mixer) {
-//       group.current.mixer.update(delta);
-//     }
-//     group.current.position.x += speed;
-//   });
-
-//   const { camera, gl } = useThree();
-//   camera.position.set(cam[0],cam[1],cam[2]);
-
-//   const loader = new GLTFLoader();
-//   const modelPath = `/${src}/scene.gltf`;
-
-//   loader.load(
-//     modelPath,
-//     (gltf) => {
-//       const model = gltf.scene;
-
-//       if (gltf.animations && gltf.animations.length > 0) {
-//         const animation = gltf.animations[0];
-//         const mixer = new THREE.AnimationMixer(model);
-//         const action = mixer.clipAction(animation);
-//         action.play();
-
-//         group.current.mixer = mixer;
-//         group.current.children.forEach((child) => {
-//           if (child !== model) {
-//             group.current.remove(child);
-//           }
-//         });
-//       }
-//       group.current.add(model);
-//     },
-//     undefined,
-//     (error) => {
-//       console.error('Error loading 3D model', error);
-//     }
-//   );
-
-//   return (
-//     <>
-//       <group ref={group} scale={scale}/>
-//       <orbitControls args={[camera, gl.domElement]}
-//         enableDamping={false}
-//         enableZoom={false}
-//         enablePan={false}
-//         enableRotate={false}/>
-//     </>
-//   );
-// };
-
-// const Object = ({src, speed, scale, cam}) => {
-//   return (
-//     <Canvas>
-//       <pointLight position={[10, 10, 10]} intensity={10}/>
-//       <Model src={src} speed={speed} scale={scale} cam={cam}/>
-//     </Canvas>
-//   );
-// }
-
+import { useState } from "react"
 
 function App() {
   return (
     <div id='App'>
-      <img src="./src/assets/closeup.jpg" alt="closeup" className='closeup f700'/>
+      <img src="./src/assets/closeup.jpg" alt="closeup" className='closeup prl700'/>
 
-      <nav className="navbar navbar-expand-lg">
-        <a className="navbar-brand" href="#">
-          <img src="./src/assets/tekkom.png" alt="" />
-        </a>
+      <nav className="navbar">
         <div className="collapse navbar-collapse" id="navbarNav">
           <div className="navbar-nav">
-            <div className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <div className="nav-item active" id="home">
+              <span className="nav-link" href="#">Home</span>
             </div>
             <div className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <span className="nav-link" href="#">Features</span>
             </div>
             <div className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <span className="nav-link" href="#">Pricing</span>
             </div>
           </div>
         </div>
@@ -99,14 +26,14 @@ function App() {
 
       <div id='jumbotron'>
         <div>
-          <div className='main f1300'>
+          <div className='main prl1300'>
             <h1 className='head'>
               Tekkom.3D
             </h1>
             <h3 className='tagline'>PMM - Kelompok 5</h3>
           </div>
 
-          <div className="go-next f1300">
+          <div className="go-next prl1300">
             <a  href='#about'>
               <div className='ico'>
                 <i className="bi bi-caret-down"></i>
@@ -115,7 +42,9 @@ function App() {
           </div>
         </div>
       </div>
-      <div id="about"></div>
+      <div id="about">
+
+      </div>
       <div id="exp"></div>
     </div>
   )
