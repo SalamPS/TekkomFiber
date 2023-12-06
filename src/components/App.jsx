6fa37e4f5dee100acfe1261d1d856ef36f18888e
LamP <senzawa.g@gmail.com>
2023-12-06 16:10:37 +0700

    feat: UI Update for Landing Page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,13 @@
 
 import { useState } from "react"
 
+const parts = [
+  {name : "Virtual Graphics Array", src:"vga0", prev: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  {name : "Virtual Graphics Array", src:"vga1", prev: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  {name : "Virtual Graphics Array", src:"vga2", prev: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  {name : "Virtual Graphics Array", src:"vga3", prev: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+]
+
 function App() {
   return (
     <div id='App'>
@@ -12,13 +19,13 @@ function App() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <div className="navbar-nav">
             <div className="nav-item active" id="home">
-              <span className="nav-link" href="#">Home</span>
+              <span className="nav-link" href="#">CompE</span>
             </div>
             <div className="nav-item">
-              <span className="nav-link" href="#">Features</span>
+              <span className="nav-link" href="#">ThreeFiber</span>
             </div>
             <div className="nav-item">
-              <span className="nav-link" href="#">Pricing</span>
+              <span className="nav-link" href="#">Explore</span>
             </div>
           </div>
         </div>
@@ -28,7 +35,7 @@ function App() {
         <div>
           <div className='main prl1300'>
             <h1 className='head'>
-              Tekkom.3D
+              Tekkom . 3Fiber
             </h1>
             <h3 className='tagline'>PMM - Kelompok 5</h3>
           </div>
@@ -43,9 +50,35 @@ function App() {
         </div>
       </div>
       <div id="about">
-
+        <h1 className="h1">Computer Engineering</h1>
       </div>
-      <div id="exp"></div>
+      <div id="three">
+        <div className="section block">
+          <h1 className="h1">About Three Fiber</h1>
+        </div>
+        <div className="section bg">
+          <img src="./src/assets/room.jpg" alt="room"/>
+        </div>
+      </div>
+      <div id="exp">
+        <h1 className="h1">Explore Components</h1>
+        <div className="block">
+          {
+            parts.map(part => (
+              <div key={part.src} className="item">
+                <img src={`./src/assets/${part.src}.png`} alt={part.name} />
+                <div className="preview">
+                  <div className="title">{part.name}</div>
+                  <div className="prev">{part.prev}</div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+        <div className="more">
+          <button>SHOW MORE</button>
+        </div>
+      </div>
     </div>
   )
 }
