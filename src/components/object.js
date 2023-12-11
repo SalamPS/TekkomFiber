ad2bd2928615c4ace95f7f53c86894f30e80d59d
LamP <senzawa.g@gmail.com>
2023-12-11 08:35:58 +0700

    chore: Re-Init

---
+++ src/components/object.js
@@ -0,0 +1,11 @@
+class object {
+  constructor (type,name,desc,prop,origin) {
+    this.type = type;
+    this.name = name;
+    this.desc = desc;
+    this.prop = prop;
+    this.origin = origin;
+  }
+}
+
+export default object;
