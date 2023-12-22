f82a10cd3e455147c0676449a56c3ba55773d65b
LamP <senzawa.g@gmail.com>
2023-12-22 22:00:08 +0700

    chore(components): UI Update

--- src/app/components/page.jsx
+++ src/app/components/page.jsx
@@ -3,13 +3,25 @@
 import { useEffect, useState } from 'react'
 import styles from './components.css'
 
+import Link from 'next/link';
 import Image from 'next/image'
 import list from '../../components/list'
 import ParticlesContainer from '../../components/ParticlesContainer'
 
-function Components({params}) {
-  const [active, setActive] = useState(-1)
+const Sector = () => {
+  
+}
 
+const Preview = () => {
+  return (
+    <div id="preview">
+      <h1 className='tag'>Tekkom.3Fiber</h1>
+      <h2 className='tag'>Dive into a dynamic visual experience, interact with intricate details, and gain insights into the inner workings of Computer components</h2>
+    </div>
+  )
+}
+
+function Components({params}) {
   const sectors = list.reduce((result, item) => {
     const group = item.sector;
     if (!result[group]) {
@@ -18,9 +30,6 @@ function Components({params}) {
     result[group].push(item);
     return result;
   }, {});
-  
-  const activate = (hit) => setActive(hit);
-  const deactivate = () => setActive(-1);
 
   return (
     <div id="Components">
@@ -32,15 +41,15 @@ function Components({params}) {
         </div>
         <div className="body">
           {Object.entries(sectors).map(([sector, item]) => (
-            <div key={sector} className='box'>
+            <Link key={sector} className='box' href={`/components/${sector}`}>
               <span className={`icon bi bi-${item[0].icon}`}></span>
               <span className="title">{sector.toUpperCase()}</span>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
       <div id="sectorPreview">
-        {params.sector ? <>Sector</> : <></>}
+        {params.sector ? <Sector/> : <Preview/>}
       </div>
     </div>
   )
