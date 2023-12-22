52f165ff378f9ef8a4ff5488ed71682eb5782718
LamP <senzawa.g@gmail.com>
2023-12-22 21:13:34 +0700

    chore(list): Added Bootstrap Icon

--- src/components/list.js
+++ src/components/list.js
@@ -1,7 +1,7 @@
 import object from './object'
 
 const list = [
-  new object('vga', 'AsusROG Geforce-RTX', 'Virtual Graphic Array', 'gpu', {
+  new object('vga', 'AsusROG Geforce-RTX', 'Virtual Graphic Array', 'gpu', 'gpu-card', {
     scale: 5,
     speed: [0.0005, 0, 0],
     light: undefined,
@@ -10,7 +10,7 @@ const list = [
     src: 'asus_rog_geforce_rtx_VGA_CARD'
   },"https://sketchfab.com/3d-models/asus-rog-geforce-rtx-4090-v20-6f527569f14b4efc94c7072842bd41ac"
   ),
-  new object('monitor', 'Asus PC Gaming Monitor', 'Gaming Monitor', 'display', {
+  new object('monitor', 'Asus PC Gaming Monitor', 'Gaming Monitor', 'display', 'tv-fill', {
     scale: 15,
     speed: [0, 0, 0],
     light: undefined,
@@ -19,7 +19,7 @@ const list = [
     src: 'asus_pc_gaming_monitor'
   },"https://sketchfab.com/3d-models/asus-pc-gaming-monitor-941ce968c30a42d0a640a5c9d7f56128"
   ),
-  new object('motherboard', 'Asus Strix B550-F Gaming Motherboard', 'Gaming Motherboard', 'motherboard', {
+  new object('motherboard', 'Asus Strix B550-F Gaming Motherboard', 'Gaming Motherboard', 'motherboard', 'motherboard-fill', {
     scale: 0.8,
     speed: [0, 0, 0],
     light: undefined,
@@ -28,7 +28,7 @@ const list = [
     src: 'asus_strix_b_550_f_gaming_motherboard_realistic'
   },"https://sketchfab.com/3d-models/asus-strix-b-550-f-gaming-motherboard-realistic-3eba5f45bed74fbeb2647de38047000f"
   ),
-  new object('ddr4', 'Crucial Ballistix 8GB DDR4 3600 RAM (Black)', 'DDR4 RAM', 'ram', {
+  new object('ddr4', 'Crucial Ballistix 8GB DDR4 3600 RAM (Black)', 'DDR4 RAM', 'ram', 'memory', {
     scale: 200,
     speed: [0, 0, 0],
     light: 15,
@@ -37,7 +37,7 @@ const list = [
     src: 'crucial_ballistix_8gb_ddr4_3600_ram__black'
   },"https://sketchfab.com/3d-models/crucial-ballistix-8gb-ddr4-3600-ram-black-1fce4935471e46cab6ee57ba140c87f9"
   ),
-  new object('cpu', 'Dream Computer CPU', 'Computer CPU', 'cpu', {
+  new object('cpu', 'Dream Computer CPU', 'Computer CPU', 'cpu', 'cpu-fill', {
     scale: 1,
     speed: [0, 0, 0],
     light: undefined,
@@ -46,7 +46,7 @@ const list = [
     src: 'dream_computer_CPU'
   },"https://sketchfab.com/3d-models/dream-computer-setup-82f78bbaf2d34f01af854a52151dbf49"
   ),
-  new object('ball', 'Gaming Mouse', 'Gaming Mouse', 'mouse', {
+  new object('ball', 'Gaming Mouse', 'Gaming Mouse', 'mouse', 'mouse3-fill', {
     scale: 1,
     speed: [0, 0, 0],
     light: undefined,
@@ -55,7 +55,7 @@ const list = [
     src: 'gaming_mouse'
   },"https://sketchfab.com/3d-models/gaming-mouse-ab744476bdb343a9bf4544db12168af6"
   ),
-  new object('mechanical', 'Mechanical RGB Keyboard', 'RGB Keyboard', 'keyboard', {
+  new object('mechanical', 'Mechanical RGB Keyboard', 'RGB Keyboard', 'keyboard', 'keyboard-fill', {
     scale: 0.3,
     speed: [0.0005, 0, 0],
     light: undefined,
@@ -64,7 +64,7 @@ const list = [
     src: 'mechanical_rgb_keyboard'
   },"https://sketchfab.com/3d-models/mechanical-rgb-keyboard-4650f5bafe934a90b9f09396b843a966"
   ),
-  new object('air', 'PC Cooler 2', 'Cooling System', 'cooling', {
+  new object('air', 'PC Cooler 2', 'Cooling System', 'cooling', 'fan', {
     scale: 1,
     speed: [0, 0, 0],
     light: undefined,
@@ -73,7 +73,7 @@ const list = [
     src: 'pc_cooler_2'
   },"https://sketchfab.com/3d-models/pc-cooler-2-5cc52b3d32064c9ca6d476e1a90b634f"
   ),
-  new object('speaker', 'Speaker Vipe Nitro X7', 'Gaming Speaker', 'speaker', {
+  new object('speaker', 'Speaker Vipe Nitro X7', 'Gaming Speaker', 'speaker', 'speaker-fill', {
     scale: 1,
     speed: [0, 0, 0],
     light: undefined,
@@ -82,7 +82,7 @@ const list = [
     src: 'speaker_vipe_nitro_x7'
   },"https://sketchfab.com/3d-models/speaker-vipe-nitro-x7-f8ad99da408f4211b224ef51f08cee06"
   ),
-  new object('ssd', 'SSD Samsung 980 Pro 1TB', 'Solid State Drive', 'storage', {
+  new object('ssd', 'SSD Samsung 980 Pro 1TB', 'Solid State Drive', 'storage', 'hdd-fill', {
     scale: 1,
     speed: [0, 0, 0],
     light: undefined,
@@ -91,7 +91,7 @@ const list = [
     src: 'ssd_samsung_980_pro_1tb'
   },"https://sketchfab.com/3d-models/ssd-samsung-980-pro-1tb-208b846d5a5948368b4ad0676bf4ab97"
   ),
-  new object('desktop', 'Desktop PC', 'Personal Computer', 'setup', {
+  new object('desktop', 'Desktop PC', 'Personal Computer', 'setup', 'pc-display', {
     scale: 1,
     speed: [0, 0, 0],
     light: undefined,
