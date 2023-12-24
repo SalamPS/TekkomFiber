affd71e9ef20f124b2d6b1b7e5bc1c5ff3556787
LamP <senzawa.g@gmail.com>
2023-12-24 19:10:42 +0700

    chore(vercel): Speed Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,10 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css"/>
       </head>
-      <body className={nova.className}>{children}</body>
+      <body className={nova.className}>
+        {children}
+        <SpeedInsights/>
+      </body>
     </html>
   )
 }
