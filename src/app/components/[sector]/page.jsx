543490e1df6c44f61ed034de1a0258b408b8f9ae
LamP <senzawa.g@gmail.com>
2023-12-24 14:23:43 +0700

    chore(sector): Removed .log

--- src/app/components/[sector]/page.jsx
+++ src/app/components/[sector]/page.jsx
@@ -6,7 +6,6 @@ export const metadata = {
 
 export default function Sector ({params}) {
   metadata.title = params.sector.length < 4 ? params.sector.toUpperCase() : `${params.sector.charAt(0).toUpperCase()}${params.sector.slice(1)}`
-  console.log(Components)
   return (
     <Components params={params}/>
   )
