bcfad7fc5380a189fb05df4a507bf46890a12f97
LamP <senzawa.g@gmail.com>
2023-12-25 13:21:21 +0700

    feat(uppercase): Optimized

--- src/components/upperCase.js
+++ src/components/upperCase.js
@@ -1,4 +1,9 @@
 export default function upperCase (text) {
+  if(text.includes("-")) {
+    text = text.split("-");
+
+    return text.map(word => (word.charAt(0).toUpperCase() + word.slice(1) + " "))
+  }
   if (text.length < 5) return text.toUpperCase()
   else return text.charAt(0).toUpperCase() + text.slice(1)
 }
