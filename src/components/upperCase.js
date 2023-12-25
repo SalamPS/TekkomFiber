998007c574b03fe020cab5dce48cd2f5ee55c94e
LamP <senzawa.g@gmail.com>
2023-12-25 14:01:07 +0700

    chore(uppercase): Optimized

--- src/components/upperCase.js
+++ src/components/upperCase.js
@@ -2,7 +2,12 @@ export default function upperCase (text) {
   if(text.includes("-")) {
     text = text.split("-");
 
-    return text.map(word => (word.charAt(0).toUpperCase() + word.slice(1) + " "))
+    return text.map(word => {
+      if (text.length < 5) word = word.toUpperCase()
+      else word = word.charAt(0).toUpperCase() + word.slice(1)
+
+      return (word.charAt(0).toUpperCase() + word.slice(1) + " ")
+    })
   }
   if (text.length < 5) return text.toUpperCase()
   else return text.charAt(0).toUpperCase() + text.slice(1)
