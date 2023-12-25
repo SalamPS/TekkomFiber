ed76afc06811181f1a2b45cf926b05eb475b9951
LamP <senzawa.g@gmail.com>
2023-12-25 20:17:11 +0700

    fix(title): Fixed bug on title that includes Spaces

--- src/components/upperCase.js
+++ src/components/upperCase.js
@@ -1,13 +1,14 @@
 export default function upperCase (text) {
   if(text.includes("-")) {
     text = text.split("-");
-
-    return text.map(word => {
+    let out = ""
+    text.forEach(word => {
       if (text.length < 5) word = word.toUpperCase()
       else word = word.charAt(0).toUpperCase() + word.slice(1)
 
-      return (word.charAt(0).toUpperCase() + word.slice(1) + " ")
+      out += word.charAt(0).toUpperCase() + word.slice(1) + " ";
     })
+    return out;
   }
   if (text.length < 5) return text.toUpperCase()
   else return text.charAt(0).toUpperCase() + text.slice(1)
